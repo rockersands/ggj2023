45307e7f5c740bafa960c422f78f02e7d0d39b97
RodrigoTerpanArenas <43326298+RodrigoTerpanArenas@users.noreply.github.com>
2023-02-03 22:09:22 -0600

    Commit 10 pm
    puras locuras

---
+++ Rootggj2023oldu/Assets/Scripts/Rodrigo/BackgroundTileMapGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BackgroundTileMapGenerator : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public TileBase[] tiles;
+
+    private int[,] mapData =
+    {
+        {1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1}
+    };
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    private void GenerateMap()
+    {
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                Vector3Int p = new Vector3Int(x, y, 0);
+                if (x == 0 || y == 0 || x == 7 || y == 7)
+                {
+                    tilemap.SetTile(p, tiles[1]);
+                }
+                else
+                {
+                    tilemap.SetTile(p, tiles[0]);
+                }
+
+            }
+        }
+    }
+}
+
---
+++ Rootggj2023oldu/Assets/Scripts/Rodrigo/ReadData.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.IO;
+
+
+public class ReadData : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string[,] ReadFromCSV(string filepath)
+    {
+        string csvFile = File.ReadAllText(filepath);
+        string[] data = csvFile.Split(new char[] { '\n' });
+
+        int rowCount = data.Length;
+        int colCount = data[0].Split(new char[] { ',' }).Length;
+
+        string[,] csvData = new string[rowCount, colCount];
+
+        for (int i = 0; i < rowCount; i++) 
+        {
+            string[] row = data[i].Split(new char[] { ',' });
+
+            for (int j = 0; j < colCount; j++) 
+            {
+                csvData[i, j] = row[j];
+            }
+        }
+
+        return csvData;
+    }
+    
+}
