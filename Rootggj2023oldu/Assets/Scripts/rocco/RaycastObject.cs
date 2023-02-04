78205cfaac31a69e442ad87d7297d79d81f2e61b
Joaquin Frausto Alfaro <joaquin.frausto@fyware.com>
2023-02-03 20:57:00 -0600

    added scene and packages

---
+++ Rootggj2023oldu/Assets/Scripts/rocco/RaycastObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastObject : MonoBehaviour
+{
+
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hitInfo.distance, Color.red);
+        Physics.Raycast(transform.position,transform.TransformDirection(Vector3.down),out RaycastHit hitInfo, 20f))
+        {
+            Debug.Log("hit something");
+        }
+    }
+}
