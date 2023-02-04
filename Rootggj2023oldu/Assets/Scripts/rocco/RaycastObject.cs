d3e87dc33725a9bc12e5d26bd4701a9ed97a5cc8
Joaquin Frausto Alfaro <joaquin.frausto@fyware.com>
2023-02-03 20:59:58 -0600

    ray

--- Rootggj2023oldu/Assets/Scripts/rocco/RaycastObject.cs
+++ Rootggj2023oldu/Assets/Scripts/rocco/RaycastObject.cs
@@ -8,7 +8,7 @@ public class RaycastObject : MonoBehaviour
     private void Update()
     {
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hitInfo.distance, Color.red);
-        Physics.Raycast(transform.position,transform.TransformDirection(Vector3.down),out RaycastHit hitInfo, 20f))
+        if( Physics.Raycast(transform.position,transform.TransformDirection(Vector3.down),out RaycastHit hitInfo, 20f))
         {
             Debug.Log("hit something");
         }
